f4f0668528daa45306159de34f13002a7beb35ff
Yuriy Chornopyskyi <yurii.chornopyskyi@intellias.com>
2022-07-05 14:16:24 +0300

    update cors

--- Program.cs
+++ Program.cs
@@ -16,7 +16,8 @@ builder.Services.AddCors(options =>
                           policy.WithOrigins(
                             "http://localhost:8080",
                             "https://icy-dune-02041e603.1.azurestaticapps.net")
-                            .WithMethods("GET", "POST", "DELETE", "PUT");
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
                       });
 });
 
